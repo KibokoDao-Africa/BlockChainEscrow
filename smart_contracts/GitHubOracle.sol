31e24d256187a6f8dedeac37684412f4a38a0c68
Gethsun 1 <gethsun09@mail.com>
2025-01-16 19:48:01 +0300

    Improved Escrow contract to track Deposited Escrow amounts for project

--- smart_contracts/GitHubOracle.sol
+++ smart_contracts/GitHubOracle.sol
@@ -1,26 +1,50 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+/// @title GitHubOracle
+/// @notice This contract acts as an off-chain oracle for receiving and storing test results 
+///         for projects deployed in a blockchain environment. The test results are mapped 
+///         to specific project IDs and can be queried by other smart contracts.
+/// @dev Designed to be used in conjunction with other smart contracts such as Escrow.
+
 contract GitHubOracle {
+    /// @notice Address of the admin authorized to fulfill oracle requests.
     address public admin;
+
+    /// @notice Mapping of project IDs to their corresponding test results.
     mapping(uint256 => string) public testResults;
 
+    /// @notice Event emitted when a new test result is received for a project.
+    /// @param projectId The ID of the project for which the test result is provided.
+    /// @param result The test result associated with the project.
     event TestResultReceived(uint256 projectId, string result);
 
+    /// @dev Ensures that only the admin can execute specific functions.
     modifier onlyAdmin() {
         require(msg.sender == admin, "Not admin");
         _;
     }
 
+    /// @notice Initializes the contract and sets the deployer as the admin.
     constructor() {
         admin = msg.sender;
     }
 
+    /// @notice Allows the admin to provide test results for a specific project.
+    /// @dev This function can only be called by the admin.
+    /// @param projectId The ID of the project for which the test result is being submitted.
+    /// @param result The test result string to be stored for the project.
     function fulfill(uint256 projectId, string calldata result) external onlyAdmin {
+        // Store the test result for the specified project ID.
         testResults[projectId] = result;
+
+        // Emit an event to notify other systems that a test result has been received.
         emit TestResultReceived(projectId, result);
     }
 
+    /// @notice Retrieves the test result for a given project.
+    /// @param projectId The ID of the project for which the test result is requested.
+    /// @return The test result string associated with the project ID.
     function getTestResult(uint256 projectId) external view returns (string memory) {
         return testResults[projectId];
     }
--- smart_contracts/RoleManagement.sol
+++ smart_contracts/RoleManagement.sol
@@ -1,38 +1,64 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+/// @title RoleManagement
+/// @notice This contract is used for managing roles (users and developers) within the system.
+/// @dev It includes functionality for role assignment and verification, with restricted access for the admin.
 contract RoleManagement {
+    /// @notice Address of the contract administrator.
     address public admin;
 
+    /// @notice Mapping to track registered users.
     mapping(address => bool) public users;
+
+    /// @notice Mapping to track registered developers.
     mapping(address => bool) public developers;
 
+    /// @notice Event emitted when a new user is registered.
+    /// @param user Address of the registered user.
     event UserRegistered(address user);
+
+    /// @notice Event emitted when a new developer is registered.
+    /// @param developer Address of the registered developer.
     event DeveloperRegistered(address developer);
 
+    /// @dev Ensures that only the admin can call certain functions.
     modifier onlyAdmin() {
         require(msg.sender == admin, "Not admin");
         _;
     }
 
+    /// @notice Initializes the contract and sets the deployer as the admin.
     constructor() {
         admin = msg.sender;
     }
 
+    /// @notice Registers a user in the system.
+    /// @dev This function can only be called by the admin.
+    /// @param user Address of the user to register.
     function registerUser(address user) external onlyAdmin {
         users[user] = true;
         emit UserRegistered(user);
     }
 
+    /// @notice Registers a developer in the system.
+    /// @dev This function can only be called by the admin.
+    /// @param developer Address of the developer to register.
     function registerDeveloper(address developer) external onlyAdmin {
         developers[developer] = true;
         emit DeveloperRegistered(developer);
     }
 
+    /// @notice Checks if an address is a registered user.
+    /// @param user Address to check.
+    /// @return True if the address is a registered user, otherwise false.
     function isUser(address user) external view returns (bool) {
         return users[user];
     }
 
+    /// @notice Checks if an address is a registered developer.
+    /// @param developer Address to check.
+    /// @return True if the address is a registered developer, otherwise false.
     function isDeveloper(address developer) external view returns (bool) {
         return developers[developer];
     }
